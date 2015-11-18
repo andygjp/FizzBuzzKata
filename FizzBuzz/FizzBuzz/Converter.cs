9109de5187368cc946acb075f3a122a3ff64516e
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 11:28:46 +0000

    More refactoring.

--- FizzBuzz/FizzBuzz/Converter.cs
+++ FizzBuzz/FizzBuzz/Converter.cs
@@ -1,20 +1,22 @@
 namespace FizzBuzz
 {
+    using System.Linq;
+
     public class Converter
     {
         private readonly NumberToString _fizzer = new Fizzer();
         private readonly NumberToString _buzzer = new Buzzer();
         private readonly Fallback _fallback = new Fallback();
+        private readonly IConvert[] _converters;
+
+        public Converter()
+        {
+            _converters = new IConvert[] {_fizzer, _buzzer, _fallback};
+        }
 
         public string Convert(int input)
         {
-            string returnValue = "";
-            var converters = new IConvert[] {_fizzer, _buzzer, _fallback};
-            foreach (var converter in converters)
-            {
-                returnValue = ReturnValue(input, returnValue, converter);
-            }
-            return returnValue;
+            return _converters.Aggregate("", (current, converter) => ReturnValue(input, current, converter));
         }
 
         private string ReturnValue(int input, string returnValue, IConvert converter)
