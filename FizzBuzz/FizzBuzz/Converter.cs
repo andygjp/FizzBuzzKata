26dbb79f98e62038c12806fb73ece78ce37463b3
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 10:46:14 +0000

    More refactoring.

--- FizzBuzz/FizzBuzz/Converter.cs
+++ FizzBuzz/FizzBuzz/Converter.cs
@@ -2,6 +2,14 @@ namespace FizzBuzz
 {
     using System;
 
+    public class MathHelper
+    {
+        public static bool AnyRemainders(int leftOperand, int rightOperand)
+        {
+            return leftOperand % rightOperand == 0;
+        }
+    }
+
     public class thing
     {
         public string AppendValueToReturnValueIfConditionIsMet(int input, string returnValue, Predicate<int> predicate, string value)
@@ -12,22 +20,18 @@ namespace FizzBuzz
             }
             return returnValue;
         }
-
-        public bool AnyRemainders(int leftOperand, int rightOperand)
-        {
-            return leftOperand % rightOperand == 0;
-        }
     }
 
     public class Converter
     {
-        private readonly thing _thing = new thing();
+        private readonly thing _fizzer = new thing();
+        private readonly thing _buzzer = new thing();
 
         public string Convert(int input)
         {
             string returnValue = "";
-            returnValue = _thing.AppendValueToReturnValueIfConditionIsMet(input, returnValue, input1 => _thing.AnyRemainders(input1, 3), "Fizz");
-            returnValue = _thing.AppendValueToReturnValueIfConditionIsMet(input, returnValue, input2 => _thing.AnyRemainders(input2, 5), "Buzz");
+            returnValue = _fizzer.AppendValueToReturnValueIfConditionIsMet(input, returnValue, input1 => MathHelper.AnyRemainders(input1, 3), "Fizz");
+            returnValue = _buzzer.AppendValueToReturnValueIfConditionIsMet(input, returnValue, input2 => MathHelper.AnyRemainders(input2, 5), "Buzz");
             returnValue = SetReturnValueIfEmptyToInput(input, returnValue);
             return returnValue;
         }
