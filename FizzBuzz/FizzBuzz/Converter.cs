f2fda64ba6fc56aa7995e3be4a9cdd3c87c0b40f
Andrew Pattison <andygjp@hotmail.com>
2015-11-18 10:38:34 +0000

    More refactoring. I don't know what it does, so the new class is called 'thing' for now. Work in progress.

--- FizzBuzz/FizzBuzz/Converter.cs
+++ FizzBuzz/FizzBuzz/Converter.cs
@@ -2,23 +2,43 @@ namespace FizzBuzz
 {
     using System;
 
-    public class Converter
+    public class thing
     {
-        public string Convert(int input)
-        {
-            string returnValue = "";
-            returnValue = AppendValueToReturnValueIfConditionIsMet(input, returnValue, ShouldReturnFizz, "Fizz");
-            returnValue = AppendValueToReturnValueIfConditionIsMet(input, returnValue, ShouldReturnBuzz, "Buzz");
-            returnValue = SetReturnValueIfEmptyToInput(input, returnValue);
-            return returnValue;
-        }
-
-        private static string AppendValueToReturnValueIfConditionIsMet(int input, string returnValue, Predicate<int> predicate, string value)
+        public string AppendValueToReturnValueIfConditionIsMet(int input, string returnValue, Predicate<int> predicate, string value)
         {
             if (predicate(input))
             {
                 returnValue += value;
             }
+            return returnValue;
+        }
+
+        public bool ShouldReturnFizz(int input)
+        {
+            return AnyRemainders(input, 3);
+        }
+
+        public bool ShouldReturnBuzz(int input)
+        {
+            return AnyRemainders(input, 5);
+        }
+
+        private bool AnyRemainders(int leftOperand, int rightOperand)
+        {
+            return leftOperand % rightOperand == 0;
+        }
+    }
+
+    public class Converter
+    {
+        private readonly thing _thing = new thing();
+
+        public string Convert(int input)
+        {
+            string returnValue = "";
+            returnValue = _thing.AppendValueToReturnValueIfConditionIsMet(input, returnValue, _thing.ShouldReturnFizz, "Fizz");
+            returnValue = _thing.AppendValueToReturnValueIfConditionIsMet(input, returnValue, _thing.ShouldReturnBuzz, "Buzz");
+            returnValue = SetReturnValueIfEmptyToInput(input, returnValue);
             return returnValue;
         }
 
@@ -29,21 +49,6 @@ namespace FizzBuzz
                 returnValue = input.ToString();
             }
             return returnValue;
-        }
-
-        private static bool ShouldReturnFizz(int input)
-        {
-            return AnyRemainders(input, 3);
-        }
-
-        private static bool ShouldReturnBuzz(int input)
-        {
-            return AnyRemainders(input, 5);
-        }
-
-        private static bool AnyRemainders(int leftOperand, int rightOperand)
-        {
-            return leftOperand % rightOperand == 0;
         }
     }
 }
